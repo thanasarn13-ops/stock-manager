572d03bac0c22b8069fb3c631f0724d092ebca3c
thanasarn13-ops <thanasarn13@gmail.com>
2026-08-05 10:09:42 +0700

    Add files via upload

---
+++ sw (3).js
@@ -0,0 +1,5 @@
+const CACHE = 'stock-v6';
+const FILES = ['/stock-manager/','/stock-manager/index.html','/stock-manager/manifest.json','/stock-manager/icon-192.png','/stock-manager/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>{if(e.request.url.includes('index.html')){return fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(ch=>ch.put(e.request,c));return res;}).catch(()=>cached);}if(cached)return cached;return fetch(e.request).then(res=>{if(res&&res.status===200){const c=res.clone();caches.open(CACHE).then(ch=>ch.put(e.request,c));}return res;}).catch(()=>cached);}));});
